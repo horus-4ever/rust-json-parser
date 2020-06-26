0ad417e8bb9278d94968d8423d7d13bcc0692acd
horus-4ever <th.pichard54@gmail.com>
2020-06-26 10:50:49 +0200

    Updated main.rs : added example

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,22 @@ mod token;
 mod lexer;
 mod parser;
 mod json_syntax;
+mod tests;
 
 use lexer::Lexer;
 use parser::Parser;
+use json_syntax::*;
+
+use std::io::{Read};
+use std::fs::{File};
 
 fn main() {
-    let code = r#"{
-        "first_name": "Jon",
-        "number": 20,
-        "boolean": [
-            true,
-            "hi",
-            30,
-            [
-                "empty",
-            ]
-        ],
-        "other": [
-            {
-                "nope": 30,
-            }
-        ]
-    }"#;
-    let mut lex = Lexer::new(code);
-    let result = lex.tokenize().unwrap();
-    // println!("{:?}", result);
-    let parse = Parser::new(result);
-    let result = parse.parse().unwrap();
-    let lst = result.get("boolean").unwrap();
-    println!("{:?}", lst)
+    let mut handle = File::open("examples/file1.json").unwrap();
+    let mut content = String::new();
+    handle.read_to_string(&mut content).expect("Something went wrong while reading the file");
+    // lexe and parse
+    let result = Parser::new( Lexer::new(&content).tokenize().unwrap() ).parse().unwrap();
+    for item in result["phone numbers"].unwrap_list() {
+        println!("{:?}", item["number"]);
+    }
 }
